7be4d3448a3da87be6ccfb9cef84fff80dcd7199
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2021-11-02 00:30:08 +0300

    Initial Commit

---
+++ CryptoTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoTracker
+//
+//  Created by Николай Никитин on 02.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
